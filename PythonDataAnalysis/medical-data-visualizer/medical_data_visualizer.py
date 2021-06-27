97fdef48c295fb14a611f7e21509903f4c104405
Sean Coughlin <seanmc4@illinois.edu>
2021-06-26 23:12:32 -0600

    Work on med visualizer

--- PythonDataAnalysis/medical-data-visualizer/medical_data_visualizer.py
+++ PythonDataAnalysis/medical-data-visualizer/medical_data_visualizer.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv("medical_examination.csv",delimiter=',')
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = df["weight"] / (df["height"] ** 2)
+df['overweight'] = np.where(df['overweight'] >= 25, 1, 0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df["cholesterol"] = np.where((df["cholesterol"] == 1), 0, 1)
+df["gluc"] = np.where((df["gluc"] == 1), 0, 1)
 
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=['active', 'alco', "cholesterol" , 'gluc', 'overweight', 'smoke'] )
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
     df_cat = None
 
     # Draw the catplot with 'sns.catplot()'
+    figure = sns.catplot(x= "variable", kind ="count" ,hue = "value", data = df_cat, col= "cardio")
 
+    figure.set_axis_labels("variable", "total")
 
+    fig = figure.fig
 
     # Do not modify the next two lines
-    fig.savefig('catplot.png')
-    return fig
+    #fig.savefig('catplot.png')
+    #return fig
 
 
 # Draw Heat Map
@@ -44,12 +49,12 @@ def draw_heat_map():
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    #fig, ax = None
 
     # Draw the heatmap with 'sns.heatmap()'
 
 
 
     # Do not modify the next two lines
-    fig.savefig('heatmap.png')
-    return fig
+    #fig.savefig('heatmap.png')
+    #return fig
