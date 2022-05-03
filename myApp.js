0c456370ffd9980480419d22b367f943fb90f942
Sean Coughlin <coughlinscc33@gmail.com>
2022-05-03 16:56:42 -0500

    Add middleware for css support

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 
 absolutePath = __dirname + "/views/index.html"
+static = __dirname + "/public/"
+
+app.use('/public', express.static(static))
 
 app.get('/', function(req, res) {
   res.sendFile(absolutePath);
